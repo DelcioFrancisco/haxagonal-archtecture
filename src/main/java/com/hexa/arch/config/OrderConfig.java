391211576d881cff73510e1a8e2b38aac58a08ae
DelcioFrancisco <delciofrancisco_1@hotmail.com>
2025-11-22 13:20:14 +0000

    feat: config layer defination

---
+++ src/main/java/com/hexa/arch/config/OrderConfig.java
@@ -0,0 +1,16 @@
+package com.hexa.arch.config;
+
+import com.hexa.arch.domain.port.output.OrderRepositoryPort;
+import com.hexa.arch.domain.service.OrderService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfig {
+
+    @Bean
+    public OrderService orderService(OrderRepositoryPort repositoryPort) {
+        return new OrderService(repositoryPort);
+    }
+
+}
